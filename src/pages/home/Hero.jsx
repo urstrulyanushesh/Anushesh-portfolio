788d9389f78c96370e640b4c63053f87f9886a4b
Anushesh K K Tharu <143109059+urstrulyanushesh@users.noreply.github.com>
2026-09-25 15:09:55 +0545

    Update Hero.jsx

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       <div className="container hero-inner">
         <div className="hero-content">
           <h2 className="hero-title">
-             <h6 > <strong> Hello, world. I am — </strong></h6> <br />
+            <strong> Hello, world. I am — </strong> <br />
             Anushesh, 
             <span style={{ textDecoration: 'none' }} className="accent-text">Full-stack <br /> developer </span>
          
